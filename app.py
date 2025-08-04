f79bee8c4453d77ff02796bbb23b4e6a2a81116b
Damien Turner <damien.turner@my.jcu.edu.au>
2025-08-04 16:21:42 +1000

    Add initial flask project with challenge modifications

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():  # put application's code here
+    return '<h1>Hello World :)</h1'
+
+
+@app.route('/greet')
+@app.route('/greet/<name>')
+def greet(name=""):
+    return f"Hello {name}"
+
+
+@app.route('/c2f/<input_temp>')
+def web_print_c2f(input_temp=''):
+    return f"{input_temp}C is equal to {convert_c_to_f(float(input_temp))}F"
+
+
+@app.route('/f2c/<input_temp>')
+def web_print_f2c(input_temp=''):
+    return f"{input_temp}F is equal to {convert_f_to_c(float(input_temp))}C"
+
+
+def convert_c_to_f(celsius):
+    """Convert Celsius to Fahrenheit."""
+    return celsius * 9 / 5 + 32
+
+
+def convert_f_to_c(fahrenheit):
+    """Convert fahrenheit to celsius"""
+    return 5 / 9 * (fahrenheit - 32)
+
+
+if __name__ == '__main__':
+    app.run()
